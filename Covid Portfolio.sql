d76d9dce7ee3e98220e90ce2447bd1bb3c02d962
OpPizza <67818693+OpPizza@users.noreply.github.com>
2024-06-03 17:33:52 +0800

    Update Covid Portfolio.sql

--- Covid Portfolio.sql
+++ Covid Portfolio.sql
@@ -1,28 +1,29 @@
+-- Select everything and order by location and date.
 SELECT * FROM PortfolioProject..CovidDeaths
 ORDER BY 3, 4;
-
---SELECT * FROM PortfolioProject..CovidVaccinations
---ORDER BY 3, 4;
 
 SELECT location, date, total_cases, new_cases, total_deaths, population FROM PortfolioProject..CovidDeaths
 ORDER BY 1, 2;
 
---SELECT location, date, total_cases, total_deaths, (CONVERT(FLOAT, total_deaths) / NULLIF(CONVERT(FLOAT, total_cases), 0)) * 100 AS Deathpercentage
---from PortfolioProject..covidDeaths
---order by 1,2;
-
+-- Total Cases vs Total Deaths
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 AS DeathPercentage
 from PortfolioProject..covidDeaths
 WHERE continent IS NOT NULL
 order by 1,2;
 
--- Total Cases vs Total Deaths
+-- Total Cases vs Total Deaths in Malaysia
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 AS DeathPercentage
 from PortfolioProject..covidDeaths
 WHERE location = 'Malaysia'
 ORDER BY 1,2;
 
 -- Total Cases vs Population
+SELECT location, date, population, total_cases, (total_cases/population) * 100 AS InfectedPercentage
+FROM PortfolioProject..covidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2;
+
+-- Total Cases vs Population in Malaysia
 SELECT location, date, population, total_cases, (total_cases/population) * 100 AS InfectedPercentage
 FROM PortfolioProject..covidDeaths
 WHERE location = 'Malaysia'
@@ -69,7 +70,6 @@ SELECT PortfolioProject..CovidDeaths.continent, PortfolioProject..CovidDeaths.lo
 PortfolioProject..CovidDeaths.population, PortfolioProject..CovidVaccinations.new_vaccinations,
 SUM(CAST(CovidVaccinations.new_vaccinations AS INT)) 
 OVER (PARTITION BY PortfolioProject..CovidDeaths.location ORDER BY PortfolioProject..CovidDeaths.location, PortfolioProject..CovidDeaths.date) AS Vaccinated
---(Vaccinated / population)*100
 FROM PortfolioProject..CovidDeaths
 JOIN PortfolioProject..CovidVaccinations ON PortfolioProject..CovidDeaths.location = PortfolioProject..CovidVaccinations.location 
 AND 
@@ -92,35 +92,8 @@ JOIN PortfolioProject..CovidVaccinations ON PortfolioProject..CovidDeaths.locati
 AND 
 PortfolioProject..CovidDeaths.date = PortfolioProject..CovidVaccinations.date
 WHERE PortfolioProject..CovidDeaths.continent IS NOT NULL
---ORDER BY 2, 3
 )
 SELECT *, (Vaccinated/population)*100 AS vaccinated_percentage FROM PopvsVac;
-
----- Temp Table
-
---CREATE TABLE #VaccinatedPercentage
---(
---continent nvarchar(255),
---location nvarhcar(255),
---date DATETIME,
---population NUMERIC,
---new_vaccination NUMERIC,
---vaccinated NUMERIC
---)
-
-
---INSERT INTO #VaccinatedPercentage
---SELECT PortfolioProject..CovidDeaths.continent, PortfolioProject..CovidDeaths.location, PortfolioProject..CovidDeaths.date, 
---PortfolioProject..CovidDeaths.population, PortfolioProject..CovidVaccinations.new_vaccinations,
---SUM(CAST(CovidVaccinations.new_vaccinations AS INT)) 
---OVER (PARTITION BY PortfolioProject..CovidDeaths.location ORDER BY PortfolioProject..CovidDeaths.location, PortfolioProject..CovidDeaths.date) AS Vaccinated
---FROM PortfolioProject..CovidDeaths
---JOIN PortfolioProject..CovidVaccinations ON PortfolioProject..CovidDeaths.location = PortfolioProject..CovidVaccinations.location 
---AND 
---PortfolioProject..CovidDeaths.date = PortfolioProject..CovidVaccinations.date
---WHERE PortfolioProject..CovidDeaths.continent IS NOT NULL
-----ORDER BY 2, 3
-
 
 -- Create VIEW
 
